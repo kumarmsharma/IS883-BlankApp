7ad1aa20d2c8e2cc40a241a0034995e5a9ce9450
kumarmsharma <156374440+kumarmsharma@users.noreply.github.com>
2024-10-15 15:41:49 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,5 @@ import streamlit as st
 
 st.title("🎈 My new application")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "Let's never start building! For help and demotivation, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
