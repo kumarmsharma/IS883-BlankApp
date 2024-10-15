f63c744918bf9dae477cfc30aca269fb2b96173f
kumarmsharma <156374440+kumarmsharma@users.noreply.github.com>
2024-10-15 15:42:28 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new application")
+st.title("🎈 Rae's Korean Ramen Shop")
 st.write(
-    "Let's never start building! For help and demotivation, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "To eat and overload, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
